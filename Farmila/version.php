bd675d838daf72592efd955ff97f012d5268ff86
Arthdbd <70667884+Arthdbd@users.noreply.github.com>
2025-02-24 23:42:05 +0200

    Update version.php

--- Farmila/version.php
+++ Farmila/version.php
@@ -1,7 +1,1 @@
-{
-    "status": "success",
-    "update_available": true,
-    "version": "7.2.8",
-    "update_url": "https://kead.top/Farmila/update.zip",
-    "message": "Доступно новое обновление."
-}
+
